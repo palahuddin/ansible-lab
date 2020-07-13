f40739c0f6fcb904aa77124fb1474382f02981cd
palahuddin <ap.falah@gmail.com>
2020-07-13 18:35:35 +0700

    ansible

--- tools.sh
+++ tools.sh
@@ -1,1 +1,2 @@
-yum update ; yum install -y epel-release ; yum install -y rsync screen zip unzip wget curl
+yum update ; yum install -y epel-release ; 
+yum install -y rsync screen zip unzip wget curl net-tools
